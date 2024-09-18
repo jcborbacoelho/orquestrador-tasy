d06f490a6e2b42460d1270267e6a569e6a679e43
Julio Cézar <julio.ciec@gmail.com>
2024-09-18 09:31:46 -0400

    Adicionando IBM Storage

---
+++ src/watson/s3/s3.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@nestjs/common';
+import * as IBM from 'ibm-cos-sdk'
+
+@Injectable()
+export class S3Service {
+    private config
+
+    constructor() {
+        this.config = {
+            endpoint: process.env.IBM_ENDPOINT,  // Endereço do seu COS
+            apiKeyId: process.env.IBM_APIKEY,   // Sua API Key do IBM Cloud
+            serviceInstanceId: process.env.IBM_INSTANCE_ID, // ID da instância do seu COS
+        }
+    }
+
+    async uploadFile(buffer, filename) {
+        const cos = new IBM.S3(this.config)
+        const params = {
+            Bucket: 'tasy-audio',  // Nome do bucket
+            Key: `${filename}.ogg`,      // Nome do arquivo
+            Body: buffer,
+            ContentType: 'audio/ogg',   // Tipo de conteúdo
+            ACL: 'public-read',         // Deixe o arquivo público para acesso
+        };
+
+        try {
+            const data = await cos.upload(params).promise();
+
+            console.log("IBM S3")
+            console.log(data)
+
+            return `https://${params.Bucket}.${this.config.endpoint}/${params.Key}`; // URL pública do arquivo
+        } catch (err) {
+            console.error('Erro ao fazer upload no IBM Cloud Object Storage:', err);
+            throw err;
+        }
+    }
+}
--- src/watson/watson.module.ts
+++ src/watson/watson.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { WatsonService } from './watson.service';
+import { S3Service } from './s3/s3.service';
 
 @Module({
-  providers: [WatsonService],
+  providers: [WatsonService, S3Service],
   exports: [WatsonService],
 })
 export class WatsonModule {}
